b20a525e3463316323dff4e1ba83ead95d58f755
Delton Ding <dsh0416@gmail.com>
2016-06-26 04:08:37 +0800

    Update code

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'bundler'
+gem 'rake'
+gem 'puma'
+gem 'sinatra', '>=1.4.7', require:'sinatra/base'
+gem 'faye-websocket', require:'faye/websocket'
+gem 'json'
---
+++ main.rb
@@ -0,0 +1,81 @@
+require 'sinatra'
+require 'faye/websocket'
+require 'json'
+require 'net/http'
+
+#configure {set :server, :puma}
+Faye::WebSocket.load_adapter('thin')
+
+$data = nil
+$sockets_seatbelt = []
+$sockets_airbag_crash = []
+$sockets_airbag = []
+$sockets_location = []
+$sockets_speed = []
+
+get '/auth_test' do
+  {status: 'ok'}.to_json
+end
+
+get '/permission' do
+  {status: 'ok',  permissions: [0, 1]}.to_json
+end
+
+get '/event' do
+  ws = Faye::WebSocket.new(request.env)
+
+  ws.on :open do |event|
+
+  end
+
+  ws.on :message do |event|
+    #puts event.data
+    data = JSON.parse(event.data)['filters']
+    data.map do |filter|
+      puts filter['name']
+      case filter['name']
+        when 'seatbelt'
+          $sockets_seatbelt.push(ws)
+        when 'inhibited'
+          $sockets_airbag.push(ws)
+        when 'location'
+          $sockets_location.push({ws: ws, lat: filter['range'][0], lon: filter['range'][1]})
+        when 'speed'
+          $sockets_speed.push(ws)
+        when 'crash'
+          $sockets_airbag_crash.push(ws)
+        else
+
+      end
+    end
+  end
+
+  ws.on :close do
+    EM.next_tick do
+      $sockets_seatbelt.delete(ws)
+      $sockets_airbag.delete(ws)
+      $sockets_airbag_crash.delete(ws)
+      $sockets_speed.delete(ws)
+      $sockets_location.delete_if do |x|
+        x[:ws] == ws
+      end
+    end
+  end
+
+  ws.rack_response
+end
+
+Thread.new do
+  signal_list = %w(DriverBeltFastened AirbagCrashInfoDetected PassengerAirbagInhibited InstantSpeed)
+  while true
+    begin
+      $data = JSON.parse(Net::HTTP.get('area2.mobilecinq.com', '/ceasimulator4/getData.php', port = 8089))['records'].select do |signal|
+        signal_list.include?(signal['signal']) || (signal['signal'] == 'GPS' && signal['signal2'] == 'Current')
+      end
+      puts $data.to_json
+      sleep 10
+    rescue Exception => e
+      puts e
+    end
+  end
+end
